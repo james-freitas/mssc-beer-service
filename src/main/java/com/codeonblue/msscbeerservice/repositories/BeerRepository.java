e02c86b349277096dadfc327f9ee1f75f074357e
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2020-01-03 16:06:18 -0300

    Adding beer repository

---
+++ src/main/java/com/codeonblue/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.codeonblue.msscbeerservice.repositories;
+
+import com.codeonblue.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
